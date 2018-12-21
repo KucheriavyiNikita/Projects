5be88b7b947d270dbf8e20b3338ffc69787baec1
KucheriavyiNikita <44288072+KucheriavyiNikita@users.noreply.github.com>
2018-12-21 11:43:10 +0600

    Add files via upload

---
+++ Project0108/src/Main.java
@@ -0,0 +1,54 @@
+import processing.core.PApplet;
+
+import javax.swing.*;
+
+public class Main extends PApplet {
+
+    int a;
+    int b;
+    float x,z;
+    float y;
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+        a = 0;
+        b = 0;
+        x = 0;
+        z = 0;
+        y = 0;
+        noStroke();
+    }
+
+
+    public void draw() {
+        background(0);
+        fill(255,255,0);
+        ellipse(width/2,height/2,100,100);
+        pushMatrix();
+        fill(0,50,255);
+        translate(width/2,height/2);
+        rotate(y);
+        ellipse(200,200,40,40);
+        popMatrix();
+        pushMatrix();
+        fill(255,0,0);
+        translate(width/2,height/2);
+        rotate(y);
+        fill(255,255,255);
+        translate(200,200);
+        rotate(z);
+        ellipse(50,50,15,15);
+        popMatrix();
+        z += radians(3);
+        x += radians(2);
+        y += radians(1);
+    }
+
+    public static void main(String... args) {
+
+        PApplet.main("Main");
+    }
+}
